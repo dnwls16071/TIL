b10df0888eb19653d932e9999d8d4c972385b0bf
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-03-04 20:19:02 +0900

    원기둥 클래스 정의하기

---
+++ Java/원기둥 클래스 정의하기.java
@@ -0,0 +1,28 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        // 객체 생성
+        Cylinder c = new Cylinder();
+
+        // 속성 값 입력
+        c.radius = 4;
+        c.height = 5;
+
+        // 출력
+        System.out.printf("원기둥의 부피: %.2f\n", c.getVolume());
+        System.out.printf("원기둥의 겉넓이: %.2f\n", c.getArea());
+    }
+}
+
+class Cylinder {
+    /* 필드와 메소드를 구현하시오. */
+    int radius;
+    int height;
+
+    public double getVolume() {
+        return Math.PI * radius * radius * height;
+    }
+
+    public double getArea() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+}
