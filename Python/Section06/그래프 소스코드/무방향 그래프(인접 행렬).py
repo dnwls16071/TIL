632ac2cc74c9f17150199de61a436e76cf0838c3
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-03-11 10:23:16 +0900

    Update 무방향 그래프(인접 행렬).py

--- Python/Section06/그래프 소스코드/무방향 그래프(인접 행렬).py
+++ Python/Section06/그래프 소스코드/무방향 그래프(인접 행렬).py
@@ -1,1 +1,36 @@
+import sys
+input = sys.stdin.readline
 
+N, M = map(int, input().split())
+adjMatrix = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for _ in range(M):
+    s, e = map(int, input().split())
+    # s와 e사이에 간선의 존재 여부를 나타냄(1이면 연결O, 0이면 연결X)
+    adjMatrix[s][e] = 1
+    adjMatrix[e][s] = 1
+
+for i in range(N+1):
+    for j in range(N+1):
+        print(adjMatrix[i][j], end=" ")
+    print()
+
+# 입력
+# 6 9
+# 1 2
+# 1 3
+# 2 1
+# 2 3
+# 2 5
+# 3 4
+# 4 2
+# 4 5
+# 6 4
+
+# 출력
+# 0 0 0 0 0 0 0 
+# 0 0 1 1 0 0 0
+# 0 1 0 1 1 1 0
+# 0 1 1 0 1 0 0
+# 0 0 1 1 0 1 1
+# 0 0 1 0 1 0 0
+# 0 0 0 0 1 0 0
