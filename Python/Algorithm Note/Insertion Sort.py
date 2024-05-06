f08c0f61ca6bda75a694a3e205a118b430eec911
Jang Woo Jin <106802375+dnwls16071@users.noreply.github.com>
2024-05-06 15:10:16 +0900

    Insertion Sort.py

--- Python/Algorithm Note/Insertion Sort.py
+++ Python/Algorithm Note/Insertion Sort.py
@@ -1,6 +1,11 @@
 sample = [3, 0, 1, 8, 7, 2, 5, 4, 6, 9]
 
 def insertionSort(data):
+    """
+    삽입 정렬(Insertion Sort)
+    최선의 경우 : O(N) 시간 복잡도
+    최악의 경우 : O(N^2) 시간 복잡도
+    """
     for i in range(1, len(data)):
         for j in range(i, 0, -1):
             if data[j-1] > data[j]:
